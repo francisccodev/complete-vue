a4e35b7f60a9284ca89fc282084d9c9b98a467a3
wdplusfranciscco <wdplusfranciscco@gmail.com>
2020-08-04 21:59:44 -0500

    Add 7th README.md, main.js and vue.html files.

---
+++ 07-README.md
@@ -0,0 +1,7 @@
+Examples from "The Complete Vue JS Course" by Coding Revolution
+
+Directives:
+
+v-on:click.right  <-- Modifier "right"
+
+Arguments and modifiers
---
+++ 07-main.js
@@ -0,0 +1,14 @@
+new Vue({
+	el: "#app",
+	data: {
+		name: "Francisco"		
+	},
+	methods: {
+		changeName(newName,event){
+			this.name = newName;			
+		},
+		handleForm(){
+			
+		}
+	}
+});
